d2b6e13393d53b6c57689a24a4905cd8d65e3615
mauro-sarmiento <mn.sarmiento@alumno.um.edu.ar>
2023-08-29 18:44:28 -0300

    Agregadas tiles

--- tests/test_models.py
+++ tests/test_models.py
@@ -12,44 +12,51 @@ class TestTiles(unittest.TestCase):
         self.assertEqual(tile.letter, 'A')
         self.assertEqual(tile.value, 1)
 
+    def test_tile_II(self):
+        tile = Tile('B', 2)
+        self.assertEqual(tile.letter, 'B')
+        self.assertEqual(tile.value, 2)
+
 
 class TestBagTiles(unittest.TestCase):
-    @patch('random.shuffle')
-    def test_bag_tiles(self, patch_shuffle):
+    # @patch('random.shuffle')
+    # def test_bag_tiles(self, patch_shuffle):
+    #     bag = BagTiles()
+    #     self.assertEqual(
+    #         len(bag.tiles),
+    #         5,
+    #     )
+    #     self.assertEqual(
+    #         patch_shuffle.call_count,
+    #         1,
+    #     )
+    #     self.assertEqual(
+    #         patch_shuffle.call_args[0][0],
+    #         bag.tiles,
+    #     )
+
+
+    def test_take_initial(self):
         bag = BagTiles()
+        initial_tiles = len(bag.tiles)
+        initial_take_tiles = bag.take(7)
+        self.assertEqual(
+            len(initial_take_tiles),
+            7,
+        )
         self.assertEqual(
             len(bag.tiles),
-            5,
-        )
-        self.assertEqual(
-            patch_shuffle.call_count,
-            1,
-        )
-        self.assertEqual(
-            patch_shuffle.call_args[0][0],
-            bag.tiles,
+            initial_tiles - 7,
         )
 
-
-    def test_take(self):
+    def test_put_I(self):
         bag = BagTiles()
-        tiles = bag.take(2)
-        self.assertEqual(
-            len(bag.tiles),
-            3,
-        )
-        self.assertEqual(
-            len(tiles),
-            2,
-        )
-
-    def test_put(self):
-        bag = BagTiles()
-        put_tiles = [Tile('Z', 1), Tile('Y', 1)]
+        currently_tiles = len(bag.tiles)
+        put_tiles = [Tile('A', 1), Tile('Z', 10), Tile('Y', 4)]
         bag.put(put_tiles)
         self.assertEqual(
             len(bag.tiles),
-            7,
+            currently_tiles + 3,
         )
 
 
