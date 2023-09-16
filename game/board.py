86ca949053c072035ebe6c96dcdafc0aa85e3a28
mauro-sarmiento <mn.sarmiento@alumno.um.edu.ar>
2023-09-16 10:37:36 -0300

    Creadas dos funciones en board para delegar trabajo a WriteInBoard

--- game/board.py
+++ game/board.py
@@ -34,39 +34,46 @@ class Board:
     
     def wordCurrentPoints(self):
         return self.wordPoints
-    
-    def writeInBoard(self, startX, startY, direction, word):
+
+    def initVariables(self):
         self.wordPoints = []
         self.condicionCero = False
         self.condicionUno = False
+
+    def appendValuesInWordPoints(self, letter, multiplier):
         values = BagTiles().tiles
+        self.wordPoints.append(values[letter] * multiplier)
+    
+    def writeInBoard(self, startX, startY, direction, word):
+        self.initVariables()
+        # values = BagTiles().tiles
         for letter in word:
             if self.board[startX][startY] == '4':
                 self.board[startX][startY] = letter
-                self.wordPoints.append(values[letter] * 2)
+                self.appendValuesInWordPoints(letter, 2)
 
             elif self.board[startX][startY] == '5':
                 self.board[startX][startY] = letter
-                self.wordPoints.append(values[letter] * 3)
+                self.appendValuesInWordPoints(letter, 3)
 
 
             elif self.board[startX][startY] == '0':
                 self.board[startX][startY] = letter
-                self.wordPoints.append(values[letter])
+                self.appendValuesInWordPoints(letter, 1)
                 self.condicionCero = True
 
 
             elif self.board[startX][startY] == '1':
                 self.board[startX][startY] = letter
-                self.wordPoints.append(values[letter])
+                self.appendValuesInWordPoints(letter, 1)
                 self.condicionUno = True
 
             elif self.board[startX][startY] == '*'  or letter:
                 if self.board[startX][startY] != letter:
                     self.board[startX][startY] = letter
-                    self.wordPoints.append(values[letter])
+                    self.appendValuesInWordPoints(letter, 1)
                 else:
-                    self.wordPoints.append(values[letter])
+                    self.appendValuesInWordPoints(letter, 1)
                     
             if direction == 'H':
                 startY += 1    
